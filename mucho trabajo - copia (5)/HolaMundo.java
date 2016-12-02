f9cdfa4a65ee5a9c19e843812dc713fcdd8dba6d
Gmuela <gmuela92@gmail.com>
2016-12-02 11:33:54 +0100

    commit init

---
+++ mucho trabajo - copia (5)/HolaMundo.java
@@ -0,0 +1,15 @@
+﻿package ejercicio1;
+
+/**
+ * Created by curso mañana on 28/11/2016.
+ */
+public class HolaMundo {
+    
+    public static void main(String[] args){
+        System.out.println("Hola"+ args[0] + "Paco       otro Paco hora de hacer merge");
+        System.out.println("Hola asdf");
+
+    }
+    
+    
+}
